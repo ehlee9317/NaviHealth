0ee82afe82eae492a7edd1076661a46d5f2520ea
Stephen Ro <stephenkro@gmail.com>
2021-02-02 22:44:40 -0500

    style(added applogo and style changes to SignIn)

--- screens/SignIn.js
+++ screens/SignIn.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, StyleSheet, Alert, SafeAreaView } from "react-native";
+import { View, Text, TextInput, StyleSheet, Alert, SafeAreaView, Image} from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import { DarkTheme } from "react-native-paper";
 import { signIn } from "../api/firebaseMethods";
 
 export default function SignIn({navigation}) {
@@ -23,25 +24,64 @@ export default function SignIn({navigation}) {
   };
 
   return (
-    <SafeAreaView>
-      <Text>Sign in to your account:</Text>
-
+    <SafeAreaView style={styles.container}>
+      <Text style = {styles.text}>Account Login</Text>
+      <Image
+            source={require("../assets/applogo.jpg")}
+            style={styles.image}
+            resizeMode="center"
+          ></Image>
+      <View style={{flex:1, padding:40}}>
+      <Text style={{fontSize:15, paddingBottom:10,}}>EMAIL</Text>
       <TextInput
-        placeholder="Enter your email"
+        style= {styles.input}
+        placeholder="Please enter your email"
         value={email}
         onChangeText={(email) => setEmail(email)}
         autoCapitalize="none"
       />
+      <Text style={{fontSize:15, paddingBottom:10, paddingTop:10,}}>PASSWORD</Text>
       <TextInput
-        placeholder="Enter your password"
+        style= {styles.input}
+        placeholder="Please enter your password"
         value={password}
         onChangeText={(password) => setPassword(password)}
         secureTextEntry={true}
       />
-
+      <View style={{paddingBottom:30, paddingTop:30, textAlign:"center"}}>
       <TouchableOpacity onPress={handlePress}>
-        <Text>Submit</Text>
+        <Text style={styles.login}>Login</Text>
       </TouchableOpacity>
+      </View>
+      </View>
     </SafeAreaView>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex:1, 
+    backgroundColor: "#f7fff7",
+    alignItems: "center",
+    
+  },
+  text: {
+    padding: 60, 
+    fontSize: 40,
+  },
+  input: {
+    borderBottomWidth:2, 
+    borderColor: "#456990",
+    fontSize: 20
+  },
+  image:{
+    width:250, 
+    height:250,
+    padding:20,
+  },
+  login:{
+    fontSize:30,
+    backgroundColor:"#49BEAA",
+    padding:10,
+    textAlign:"center",
+  }
+})
