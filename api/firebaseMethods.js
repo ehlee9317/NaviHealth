a23e9c8f2903b23593c3ba99f232217104640299
Kathryn Choi <71402592+khmchoi@users.noreply.github.com>
2021-02-10 09:24:48 -0800

    feat(added actual calories burned)

--- api/firebaseMethods.js
+++ api/firebaseMethods.js
@@ -42,29 +42,30 @@ export async function loggingOut() {
 }
 
 
-export async function stopNaviFirebaseHandler(acutalDistance, actualDuration, actualDurationMin, estimatedDistance, estimatedDuration) {
-   try {
-     const currentUserUID = await firebase.auth().currentUser.uid;
-     const db = firebase.firestore();
-     const userData = await (await db.collection("users").doc(currentUserUID).get()).data()
-    //  console.log('userdata------>', userData)
-     const estCaloriesBurnedPerMinute = userData.estCaloriesBurnedPerMinute
-     await db.collection("routes").doc(currentUserUID).collection("sessions").doc().set({
-       actualDistanceKm: acutalDistance.toFixed(2),
-       actualDuration: actualDuration,
-       actualDurationMin: actualDurationMin,
-       estimatedDistanceKm: estimatedDistance.toFixed(2),
-       estimatedDurationMin: estimatedDuration.toFixed(2),
-       //to fix duration
-       estCaloriesBurned: (estCaloriesBurnedPerMinute * estimatedDuration).toFixed(2),
-       created: firebase.firestore.FieldValue.serverTimestamp(),
-       date: new Date().toDateString(),
-       timeStamp: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
-     })
-    //  console.log('estCaloriesBurnedPerMinute ------->', estCaloriesBurnedPerMinute)
-   } catch (err) {
-     Alert.alert("There is something wrong!!!!", err.message);
-   }
+export async function stopNaviFirebaseHandler(actualDistance, actualDuration, actualDurationMin, estimatedDistance, estimatedDuration) {
+  try {
+    const currentUserUID = await firebase.auth().currentUser.uid;
+    const db = firebase.firestore();
+    const userData = await (await db.collection("users").doc(currentUserUID).get()).data()
+    console.log('userdata------>', userData)
+    const estCaloriesBurnedPerMinute = userData.estCaloriesBurnedPerMinute
+    await db.collection("routes").doc(currentUserUID).collection("sessions").doc().set({
+      actualDistanceKm: actualDistance.toFixed(2),
+      actualDuration: actualDuration,
+      actualDurationMin: actualDurationMin,
+      actualCaloriesBurned: Number(estCaloriesBurnedPerMinute * actualDurationMin).toFixed(2),
+      estimatedDistanceKm: estimatedDistance.toFixed(2),
+      estimatedDurationMin: estimatedDuration.toFixed(2),
+      //to fix duration
+      estCaloriesBurned: (estCaloriesBurnedPerMinute * estimatedDuration).toFixed(2),
+      created: firebase.firestore.FieldValue.serverTimestamp(),
+      date: new Date().toDateString(),
+      timeStamp: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+    })
+   //  console.log('estCaloriesBurnedPerMinute ------->', estCaloriesBurnedPerMinute)
+  } catch (err) {
+    Alert.alert("There is something wrong!!!!", err.message);
+  }
 }
 
 export async function updateProfile(email, password, lastName, dateOfBirth, firstName, weight, height) {
